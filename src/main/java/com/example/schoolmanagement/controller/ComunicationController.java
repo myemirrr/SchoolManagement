21d8fdc337b83f7421ea114b389b34b23e5a8daa
Yasin <mustafayasinemir@gmail.com>
2024-04-21 01:49:36 +0300

    Communication controller and view

--- src/main/java/com/example/schoolmanagement/controller/ComunicationController.java
+++ src/main/java/com/example/schoolmanagement/controller/ComunicationController.java
@@ -1,13 +1,14 @@
 package com.example.schoolmanagement.controller;
 
-import org.springframework.stereotype.Controller;
+import  org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ComunicationController {
 
         @GetMapping("/comunication")
-        public String comunication() {
-            return "comunication"; // Döndürülen değer, dönülecek view adını belirtir.
+        public String comunication()
+        {
+            return "comunication";
         }
     }
